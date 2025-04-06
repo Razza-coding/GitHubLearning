58cbfb530716acf859762434747021ab7274d2d1
razzaa-coding <gamesho019@gmail.com>
2025-04-06 23:41:44 +0800

    Add lesson 3 and 4. lesson 3 includes how to develop and merge a feature. lesson 4 includes how to reset a error merge or commit.

--- github_note.py
+++ github_note.py
@@ -41,3 +41,84 @@ def lesson_2():
     '''
     print(lesson_msg)
     pass
+
+def lesson_3():
+    lesson_msg = '''
+    [ How to branch out a feature develop branch and merge back to main ]
+
+    git checkout:
+        Use git checkout to create or move into to a new branch for feature developing.
+        We use "git checkout feature-1" as example.
+        After this command, our branch will become feature-1
+
+        Anything deveiop inside feature-1 branch is separate to the main branch, so you can try any wild idea here.
+    
+    develop feature-1:
+        Now we start changing code inside feature-1
+        We'll add a init process and 2 item calls in feature-1
+    
+    develop main while feature-1 is also in development
+        Use git checkout main to swith back to main branch.
+        Now all the things we added and changed will disapear (cause we are back to main).
+        Next, we shall add a part where feature-1 is gonna be called in main.
+        In the mean time, we also optimize some of the main functions.
+        remeber too commit after all the changes is done
+
+        note:
+            Calling feature-1 in main should be done in feature-1 branch, cause we dont want to break the main code
+            But here we mimic the develop process of other poeple changing main, so we change the main process here.
+
+    merge feature-1 into main after it's done
+        First, update the "main" branch before doing any merging using git pull (from github)
+        Next, we move to main branch using "git checkout main" to start our merging process.
+        And then we call the merge command "git merge feature-1"
+        
+        What will happen?
+            feature-1 and main have different main.py code. And only feature-1 have feature_1.py
+            feature_1.py will merge in by auto merge process without a question.
+            but there will be a confict at main.py
+        Resolve confict:
+            the message will show you 3 windows
+            1. form feature-1 branch (incoming)
+            2. from main branch (current)
+            3. merge result
+
+            There will be 3 options each section
+            1. use the income code
+            2. keep the current code
+            3. merge both (auto gather code from both side with alot of difference)
+
+            There's also a manual option, you can just edit int merge result window.
+        
+        After comform, you need to leave a message, just like commiting.
+        And this is all.     
+    '''
+    print(lesson_msg)
+    pass
+
+def lesson_4():
+    lesson_msg = '''
+    [ How to reset merge and commit when we forgot to add something ] 
+
+    We need to roll back to preivous commit to change the code, and then commit/merge it back using the same process.
+    git reflog:
+        Use git reflog to see the commit records, find the commit you want to go back
+    
+    git reset:
+        Use git reset to roll back all the commit we did.
+        For example "git reset --hard main@{3}", main@{3} denote the commit we want to roll back in the "git reflog" graph.
+        The flage "--hard" means we will completely reset the commit, which includes following changes:
+            1. HEAD move to main@{3}
+            2. Stage will be cleared
+            3. remove all the files that added after main@{3} commit
+        Some other mode we can use:
+            1. --soft: keeps Stage and Files
+            2. --mixed: keeps Files only
+    
+    How to reset the "reset command"
+        1. you can use git reflog to reset a reset operation
+        2. you can see your log noted that HEAD@{A} moved to HEAD{B}
+        3. just select the HEAD before you do the reset command, then you'll be fine
+    '''
+    print(lesson_msg)
+    pass
