870b7e0c62e54fe48c8cc89d0870f2c87acbf962
razzaa-coding <gamesho019@gmail.com>
2025-04-06 22:50:16 +0800

    We modified main functions, we got init and 2 process now. Merge feature-1 between processs-1 and process-2 later, thank you.

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import os
 import github_note as note
 
 # main functions here
-def main_func():
-    print("Hello")
+def main_init():
+    print("【Hello World】")
+    pass
+
+def main_process_1():
+    print("1. this is a project about how to use github")
+
+def main_process_2():
+    print("2. we just called feature-1, which is branched and developed and will merge back later")
 
 # main program
 if __name__ == "__main__":
-    main_func()
+    main_init()
+    main_process_1()
     pass
