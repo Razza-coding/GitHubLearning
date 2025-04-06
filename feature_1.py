abb65b76a95aab2059189ef63624dbfbabbaa3cc
razzaa-coding <gamesho019@gmail.com>
2025-04-06 22:30:34 +0800

    adding feature-1

---
+++ feature_1.py
@@ -0,0 +1,10 @@
+def feature_1():
+    msg = '''
+    This is a new feature.
+    We will branch out feature-1, and then merge back later.
+    '''
+    update_msg_1 = '''
+    【festure message 1】: init
+    '''
+    print(msg + update_msg_1)
+    pass
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import github_note as note
+from feature_1 import feature_1
 
 # main functions here
 def main_func():
@@ -8,4 +9,5 @@ def main_func():
 # main program
 if __name__ == "__main__":
     main_func()
+    feature_1()
     pass
