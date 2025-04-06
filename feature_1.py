65edcca12160e1a1fe8ac86c26ad7b8a380d704d
razzaa-coding <gamesho019@gmail.com>
2025-04-06 22:34:59 +0800

    pack up all funcitons, ready to merge to main.

--- feature_1.py
+++ feature_1.py
@@ -1,12 +1,12 @@
 def feature_1():
     msg = '''
-    This is a new feature.
-    We will branch out feature-1, and then merge back later.
+    This is feature-1 unit test.
     '''
     update_msg_1 = '''
-    【festure message 1】: init
-    【festure message 1】: modify item 1 (item 1)
-    【festure message 1】: add something (item 2)
+    【festure message 1】: do init
+    【festure message 1】: call item 1
+    【festure message 1】: call item 2
+    【festure message 1】: end
     '''
     print(msg + update_msg_1)
     pass
