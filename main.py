b2cda8aee13e48a4fe8965abf1dfc729df9fb858
razzaa-coding <gamesho019@gmail.com>
2025-04-13 22:53:15 +0800

    Add feature-3 into main process

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import github_note as note
 from feature_1 import feature_1
+from feature_3 import feature_3
 
 # main functions here
 def main_init():
@@ -13,10 +14,22 @@ def main_process_1():
 def main_process_2():
     print("3. we just called feature-1, which is branched and developed and will merge back later")
 
+def main_process_3():
+    print("2. calling feature-3")
+
+def main_process_4():
+    print("4. we just called feature-3, UI is updated")
+
 # main program
 if __name__ == "__main__":
     main_init()
+
     main_process_1()
     feature_1()
     main_process_2()
+
+    main_process_3()
+    feature_3()
+    main_process_4()
+    
     pass
