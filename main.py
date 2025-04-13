ce6537a446d3d0d96a69410381eb969678b06ebe
razzaa-coding <gamesho019@gmail.com>
2025-04-13 22:46:20 +0800

    Add feature-2 into main process, ready to merge.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import github_note as note
 from feature_1 import feature_1
+from feature_2 import feature_2
 
 # main functions here
 def main_init():
@@ -13,10 +14,23 @@ def main_process_1():
 def main_process_2():
     print("3. we just called feature-1, which is branched and developed and will merge back later")
 
+def main_process_3():
+    print("4. calling feature-2")
+
+def main_process_4():
+    print("5. we just called feature-2")
+
 # main program
 if __name__ == "__main__":
     main_init()
+    # feature-1
     main_process_1()
     feature_1()
     main_process_2()
+    # feature-2
+    main_process_3()
+    feature_2()
+    # feature-3
+    # end of feature-2 and feature-3
+    main_process_4()
     pass
